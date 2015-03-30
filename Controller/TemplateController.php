30fbd520c9131596224e5cab4889a7489bff2ff7
Carlos Mendoza <inhack20@gmail.com>
2015-03-29 20:15:05 -0430

    Aactualiando font awesome

--- Controller/TemplateController.php
+++ Controller/TemplateController.php
@@ -8,6 +8,11 @@ class TemplateController extends Controller
 {
     public function indexAction()
     {
+//        var_dump(\Pandco\Bundle\AppBundle\Service\Tools::phoneAsterisks("04128493833"));
+//        var_dump(\Pandco\Bundle\AppBundle\Service\Tools::bankAccountAsterisks("01234567890123456789"));
+//        var_dump(\Pandco\Bundle\AppBundle\Service\Tools::cardAsterisks("0123456789123456"));
+//        var_dump(\Pandco\Bundle\AppBundle\Service\Tools::cardAsterisks("0123456789123456",2));
+//        die;
         return $this->render('TecnocreacionesCrmTemplateBundle:Template:index.html.twig', array());
     }
 }
